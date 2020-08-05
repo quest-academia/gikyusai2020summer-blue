6e06697cf9f09af6844aefcc70a7c401b85a0dec
{SakaeShinya} <sakashin10291029@gmail.com>
2020-08-05 14:08:26 +0900

    複数代入する属性の指定

--- lara-d/app/Event.php
+++ lara-d/app/Event.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    protected $guarded = ['id'];
+    protected $fillable = ['name','detail','start_datetime','end_datetime','number_of_members','url'];
 }
