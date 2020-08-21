c02f5d23775dc2e5b0b527ad684cadde7326042c
{SakaeShinya} <sakashin10291029@gmail.com>
2020-08-21 10:10:29 +0900

    共通テンプレートの作成

---
+++ lara-d/resources/views/layouts/common.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TimeShares</title>
+        <link href="{{asset('css/common.css')}}" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery-datetimepicker@2.5.20/jquery.datetimepicker.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-datetimepicker@2.5.20/build/jquery.datetimepicker.full.min.js"></script>
+        <script>
+            $(function(){
+                $(".datetimepicker").datetimepicker();
+            });
+        </script>
+    </head>
+    <body>
+       @yield('content')
+    </body>
+
+</hrml>
