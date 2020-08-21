ced24eddca5ad5a412803c2332a6149af631987b
{SakaeShinya} <sakashin10291029@gmail.com>
2020-08-21 10:42:29 +0900

    配列の整形、ローカルスコープの定義

--- lara-d/app/Event.php
+++ lara-d/app/Event.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    protected $fillable = ['name','detail','start_datetime','end_datetime','number_of_members','url'];
+    protected $fillable = [
+        'name',
+        'detail',
+        'start_datetime',
+        'end_datetime',
+        'number_of_members',
+        'url',
+    ];
+
+    public function scopeAscStartDatetime($query)
+    {
+        return $query
+            ->where('start_datetime', '>=', NOW())
+            ->orderBy('start_datetime', 'asc')
+            ->get();
+    }
 }
