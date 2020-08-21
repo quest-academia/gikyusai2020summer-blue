d7aa8ea0161684c9ad39798767e4d0290b47f8d6
{SakaeShinya} <sakashin10291029@gmail.com>
2020-08-21 10:47:01 +0900

    indexメソッド内でスコープメソッド呼び出し

--- lara-d/app/Http/Controllers/EventController.php
+++ lara-d/app/Http/Controllers/EventController.php
@@ -2,24 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Event;
 use App\Http\Requests\StoreEvent;
+use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function index(Request $request) {
-        $events = Event::all();
+    public function index(Request $request)
+    {
+        $events = Event::ascStartDatetime();
         return $events;
     }
 
-    public function add(Request $request) {
+    public function add(Request $request)
+    {
         return view('events.add');
     }
 
-    public function store(StoreEvent $request) {        
-        $event = new Event();                 
+    public function store(StoreEvent $request)
+    {
+        $event = new Event();
         $event->create($request->all());
         return redirect('/events');
-    } 
+    }
 }
