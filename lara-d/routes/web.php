04910fa07293b9c38da17f1e74e15225cfe22ef7
{SakaeShinya} <sakashin10291029@gmail.com>
2020-08-25 14:59:40 +0900

    詳細ページのルーティング追加

--- lara-d/routes/web.php
+++ lara-d/routes/web.php
@@ -9,13 +9,14 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/events', 'EventController@index');
+Route::get('events/{event}', 'EventController@show');
 Route::get('/events/add', 'EventController@add');
 Route::post('/events/create', 'EventController@store');
 
