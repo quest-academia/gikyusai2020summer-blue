419c7ee8acafbda3b14a591853c973a456069067
shunsuke-katsuyama <shunsuke.kty@gmail.com>
2020-08-28 15:38:03 +0000

    fourth commit

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -5,14 +5,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>TimeShares</title>
-        <link href="{{seccure_asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{seccure_asset('css/common.css')}}" rel="stylesheet">
+        <link href="{{secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{secure_asset('css/common.css')}}" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery-datetimepicker@2.5.20/jquery.datetimepicker.css">
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/jquery-datetimepicker@2.5.20/build/jquery.datetimepicker.full.min.js"></script>
         <!-- Scripts -->
-        <script src="{{seccure_asset('js/app.js') }}" defer></script>
+        <script src="{{secure_asset('js/app.js') }}" defer></script>
 
         <script>
             $(function(){
