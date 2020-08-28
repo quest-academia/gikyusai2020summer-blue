dcbb353481c6a3c51a24d58e2a91530827c1becd
shunsuke-katsuyama <shunsuke.kty@gmail.com>
2020-08-28 15:27:54 +0000

    second commit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,10 +8,6 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     public function index(Request $request)
     {
